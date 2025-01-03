3e091d0e4987c71835d2122a2d91b2b5a65806c7
Mitch Dzugan <mitchdzugan@gmail.com>
2025-01-03 14:34:51 -0600

    menu building done

--- flake.nix
+++ flake.nix
@@ -34,7 +34,6 @@
           nativeImage.extraNativeImageBuildArgs = [
             "--initialize-at-build-time"
             "-J-Dclojure.compiler.direct-linking=true"
-            "-Dskija.staticLoad=false"
             "--native-image-info"
             "-march=compatibility"
             "-H:+JNI"
@@ -45,5 +44,40 @@
             "-H:DashboardDump=target/dashboard-dump"
           ];
         };
+        packages.build-uberjar = buildZkmApp {};
+        packages.trace-run = zn.uuFlakeWrap (zn.writeBashScriptBin'
+          "trace-run"
+          ([ zkg-pkg ztr-pkg packages.build-uberjar pkgs.graalvm-ce ])
+          ''
+            gvmh="$GRAALVM_HOME"
+            if [ ! -f "$gmvh/bin/java" ]; then
+              gmvh="${pkgs.graalvm-ce}"
+            fi
+            jar_path=$(cat "${packages.build-uberjar}/nix-support/jar-path")
+            $gmvh/bin/java \
+              -agentlib:native-image-agent=config-merge-dir=./.graal-support \
+              -jar $jar_path
+          ''
+        );
+        packages.trace-normalize = zn.uuFlakeWrap (zn.writeBbScriptBin
+          "trace-normalize"
+          ''
+            (require '[cheshire.core :as json]
+                     '[clojure.string :as str])
+            (println "normalizing trace data")
+            (def trace-dir "./.graal-support/")
+            (def trace-filename (partial str trace-dir))
+            (def md (-> (trace-filename "reachability-metadata.json")
+                        slurp
+                        str/join
+                        json/parse-string))
+            (def rn-key #(-> %1 (dissoc %2) (assoc %3 (get %1 %2))))
+            (defn md-out [l md]
+              (spit (trace-filename (str l ".json")) (json/generate-string md)))
+            (md-out "jni" (map #(rn-key %1 "type" "name") (get md "jni")))
+            (md-out "resources" {"globs" (get md "resources")})
+            (println "trace data normalized. namaste and good luck =^)")
+          ''
+        );
     });
 }
--- src/zkm/core.clj
+++ src/zkm/core.clj
@@ -1,17 +1,69 @@
 (ns zkm.core
- (:require [clojure.string :as str])
+ (:require [clojure.string :as str]
+           [sci.core :as sci])
  (:gen-class))
 
-(defmacro Sub [& _] {:todo :impl})
-(defmacro Cmd [& _] {:todo :impl})
-(defmacro Col [& _] {:todo :impl})
-(defmacro Title [& _] {:todo :impl})
+(def st (atom {:next-menu-id 2
+               :curr-id 1}))
+
+(defn curr-menu [& args] (let [{:keys [menus curr-id]} (or (first args) @st)]
+                     (get menus curr-id)))
+(defn swap!-curr [f]
+  (swap! st #(assoc-in %1 [:menus (:curr-id %1)] (f (curr-menu %1)))))
+
+(defn title! [title] (swap!-curr #(assoc %1 :title title)))
+
+(defn col! [] (swap!-curr #(update %1 :cols conj (count (:entries %1)))))
+
+(defn add-entry! [& entry] (swap!-curr #(update %1 :entries conj entry)))
+
+(defn cmd! [keyspec desc exe] (add-entry! :cmd keyspec desc exe))
+
+(defn sub-begin! [keyspec desc]
+  (let [id (:next-menu-id @st)]
+    (add-entry! :sub keyspec desc id)
+    (swap! st #(-> %1
+                   (assoc-in [:menus id :parent-id] (:curr-id %1))
+                   (assoc :curr-id id)
+                   (update :next-menu-id inc)))))
+
+(defn sub-done! []
+  (swap! st #(assoc %1 :curr-id (:parent-id (curr-menu %1)))))
+
+(def Sub ^:sci/macro
+  (fn [_&form _&env keyspec desc & sub-body]
+    `(do
+       (~(symbol "sub-begin!") ~keyspec ~desc)
+       ~@sub-body
+       (~(symbol "sub-done!")))))
+
+(def Cmd ^:sci/macro
+  (fn [_&form _&env keyspec desc exe]
+    `(~(symbol "cmd!") ~keyspec ~desc ~exe)))
+
+(def Col ^:sci/macro
+  (fn [_&form _&env]
+    `(~(symbol "col!"))))
+
+(def Title ^:sci/macro
+  (fn [_&form _&env title]
+    `(~(symbol "title!") ~title)))
 
 (defn eval-file [filename]
   (let [file-content (try (slurp filename) (catch Exception _ "(-> nil)"))
         code (if-not (str/starts-with? file-content "#!") file-content
                (->> file-content str/split-lines (drop 1) (str/join \newline)))]
-    (println (eval (read-string (str "(do (in-ns 'zkm.core) " code ")"))))))
+    (sci/eval-string (str "(do " code " )")
+                     {:bindings {'title! title!
+                                 'col! col!
+                                 'cmd! cmd!
+                                 'sub-begin! sub-begin!
+                                 'sub-done! sub-done!
+                                 'Title Title
+                                 'Col Col
+                                 'Cmd Cmd
+                                 'Sub Sub}})
+    (println @st)))
 
 (defn -main [& args]
   (doseq [in-file args]
